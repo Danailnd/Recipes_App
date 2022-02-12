1ca1dc9d037b3ef2cf0a84076fcf94216f448431
Danailnd <danaildimitrov01@gmail.com>
2022-02-12 18:06:48 +0200

    Navigation !!!!!!!!!!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
 import RecipeCreationMenu from "./components/RecipeCreationMenu.jsx";
 
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <RecipeCreationMenu />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RecipeCreationMenu />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
