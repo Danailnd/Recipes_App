85ce91e2d6caa3b94d8df69eb2dc0f31fa79959e
Danailnd <danaildimitrov01@gmail.com>
2022-04-23 11:48:36 +0300

    apollo added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,22 +9,49 @@ import Fridge from "./components/fridge.jsx";
 import TestDBGraphQL from "./components/testGraphQL.jsx";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "apollo/client/link/error";
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:5000/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 function App() {
   return (
     <>
-      <NavigationBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<RecipeCreationMenu />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/IngredientsList" element={<IngredientsList />} />
-          <Route path="/Recipes" element={<Recipe />} />
-          <Route path="/addIngredient" element={<CreateNewIngredient />} />
-          <Route path="/fridge" element={<Fridge />} />
-          <Route path="/testGraphQL" element={<TestDBGraphQL />} />
-        </Routes>
-      </BrowserRouter>
+      <ApolloProvider client >
+        <NavigationBar />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<RecipeCreationMenu />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/IngredientsList" element={<IngredientsList />} />
+            <Route path="/Recipes" element={<Recipe />} />
+            <Route path="/addIngredient" element={<CreateNewIngredient />} />
+            <Route path="/fridge" element={<Fridge />} />
+            <Route path="/testGraphQL" element={<TestDBGraphQL />} />
+          </Routes>
+        </BrowserRouter>
+      </ApolloProvider>
     </>
   );
 }
